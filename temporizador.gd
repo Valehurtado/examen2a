82350218a32f9f5e52bfcd30c189043d2069bfa1
val <nanadesvalhurtado@gmail.com>
2024-06-17 11:00:48 -0600

    clase1

---
+++ temporizador.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+var minutes = 10
+var seconds = 0
+var pause = true
+
+
+func _on_ready():
+	$Timmer.one_shot = true
+	
+	
+
+func _on_boton_pressed():
+	pass # Replace with function body.
